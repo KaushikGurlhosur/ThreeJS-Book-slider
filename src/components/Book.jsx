58f7bb9676919ce6afc7757413df61dff5253b0d
Kaushik Gurlhosur <kaushikrg45@gmail.com>
2025-09-25 15:44:15 +0530

    Created a single page - using BoxGeometry

---
+++ src/components/Book.jsx
@@ -0,0 +1,47 @@
+import { useRef } from "react";
+import { pages } from "./UI";
+import { BoxGeometry } from "three";
+
+const PAGE_WIDTH = 1.28;
+const PAGE_HEIGHT = 1.71; // 4:3 aspect ratio
+const PAGE_DEPTH = 0.003;
+const PAGE_SEGMENTS = 30;
+const SEGMENT_WIDTH = PAGE_WIDTH / PAGE_SEGMENTS;
+
+const pageGeometry = new BoxGeometry(
+  PAGE_WIDTH,
+  PAGE_HEIGHT,
+  PAGE_DEPTH,
+  PAGE_SEGMENTS
+);
+
+const Page = ({ number, front, back, ...props }) => {
+  const group = useRef();
+  return (
+    <group {...props} ref={group}>
+      <mesh scale={0.1}>
+        <primitive object={pageGeometry} attach={"geometry"} />
+        <meshBasicMaterial color="red" />
+      </mesh>
+    </group>
+  );
+};
+
+const Book = ({ ...props }) => {
+  return (
+    <group>
+      {[...pages].map((pageData, index) =>
+        index === 0 ? (
+          <Page
+            key={index}
+            number={index}
+            {...pageData}
+            position-x={index * 0.15}
+          />
+        ) : null
+      )}
+    </group>
+  );
+};
+
+export default Book;
