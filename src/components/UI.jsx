331ecadee51cf50e92f406995fefae75c3deefcd
Kaushik Gurlhosur <kaushikrg45@gmail.com>
2025-09-29 13:01:25 +0530

    Single page with front cover and an image

--- src/components/UI.jsx
+++ src/components/UI.jsx
@@ -55,10 +55,10 @@ pages.push({
 export const UI = () => {
   const [page, setPage] = useAtom(pageAtom);
 
-  useEffect(() => {
-    const audio = new Audio("/audios/page-flip-01a.mp3");
-    audio.play();
-  }, [page]);
+  // useEffect(() => {
+  //   const audio = new Audio("/audios/page-flip-01a.mp3");
+  //   audio.play();
+  // }, [page]);
 
   return (
     <>
